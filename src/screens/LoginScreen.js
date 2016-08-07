89f02b4f9b617ea7335b21eae0b3c278de6d24f1
kevando <kevin@frameri.com>
2016-08-07 19:34:14 -0400

    a little janky, but this works for my timed login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -6,6 +6,8 @@ import {
   TouchableOpacity,
   StyleSheet,
   PropTypes,
+  Easing,
+  Animated
 } from 'react-native';
 import { connect } from 'react-redux';
 import * as appActions from '../reducers/app/actions';
@@ -22,40 +24,80 @@ class LoginScreen extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {loading: false, authResponse: ''}
-    this.props.navigator.toggleNavBar({
-      to: 'hidden',
-      animated: false
-    });
+    this.state = {
+      loading: false,
+      authResponse: '',
+      authenticated: false,
+      pulse: new Animated.Value(1),
+      canProceedToLogin: false,
+    }
+    this.props.navigator.toggleNavBar({to: 'hidden', animated: false });
   }
   componentWillUnmount() {
-    timer.clearTimeout('loginTimeout');
+    // clear all timers
+    timer.clearTimeout(this);
+    timer.clearInterval(this);
   }
 
   componentWillReceiveProps(nextProps) {
-    // this fires when redux updates
-    // I think I use this for updating title and such
-    console.log('new props. should have the auth data',nextProps)
+    if(nextProps.app.has('authData'))
+      this.setState({authenticated:true});
+  }
+
+  componentWillUpdate(nextProps,nextState) {
+    // console.log('next state',nextState)
+    if(nextState.canProceedToLogin && nextState.authenticated){
+      console.log('CLEAR ALL TIMERS, AND PROCEED TO LOGIN');
+      // clear all timers
+      timer.clearTimeout(this);
+      timer.clearInterval(this);
+      this.props.dispatch(appActions.changeAppRoot('after-login'));
+    }
+  }
+
+
+  pulseOnce() {
+    if (!this.props.hasPulse) {
+      return;
+    }
+
+    Animated.sequence([
+      Animated.timing(this.state.pulse, {
+        toValue: this.props.growTo,
+        duration: 300,
+        delay: 400,
+      }),
+      Animated.timing(this.state.pulse, {
+        toValue: 1,
+        easing: Easing.easeOut,
+        duration: 700,
+      }),
+    ]).start();
   }
 
   render() {
 
+    let { pulse } = this.state;
+    let animatedHeartStyles = {transform: [{scale: pulse}]}
 
     return (
-      <View style={{flex: 1, paddingTop: 130,backgroundColor: GlobalStyle.constants.colors[0],alignItems:'center'}}>
+      <View style={styles.container}>
 
         <Text style={styles.text}>
           <Text style={{fontWeight: '500',color:'#fff',fontSize:100}}>chaz</Text>
         </Text>
 
-        <TouchableOpacity style={styles.heartButton} onPress={ this.onHeartPress.bind(this) }>
-            {this.renderHeart()}
-        </TouchableOpacity>
+        <View style={styles.animationContainer}>
+          <Animated.View style={[styles.animatedView,animatedHeartStyles]}>
+          <TouchableOpacity style={styles.heartButton} onPress={ this.onHeartPress.bind(this) }>
+              {this.renderHeart()}
+          </TouchableOpacity>
+          </Animated.View>
+        </View>
 
         <Text style={styles.purpleText}>
           <Text style={{fontWeight: '100',color:'#fff'}}>{this.state.authResponse}</Text>
         </Text>
-
 
       </View>
     );
@@ -65,43 +107,41 @@ class LoginScreen extends Component {
     return <Text style={{fontSize: 90,textAlign:'center'}}><Emoji name="yellow_heart" /></Text>
   }
 
-  onHeartPress(){
-    this.setState({authResponse:'Authenticating...'});
+  onHeartPress(){ // this fn does way too much now
+    if(this.state.authResponse == 'Authenticating...')
+      return;
 
-    timer.setTimeout( 'loginTimeout',
-      () => {
-        this.setState({authResponse:'Something went wrong, probably no internet'})
-      }, 7000
-    );
+    this.pulseOnce();
+    this.setState({authResponse:'Authenticating...'});
+    //
+    timer.setTimeout( this, 'loginTimeout', () => {
+      this.setState({authResponse:'Something went wrong, probably no internet'});
+      timer.clearInterval(this,'pulseInterval');
+    } , 10000);
+
+    timer.setTimeout( this, 'minimumWait', () => {
+      this.setState({canProceedToLogin:true});
+    } , 3000);
 
     this.props.dispatch(appActions.login());
 
-    // now run an interval of the pulsing heart, every second
-    // check each second if I am authenticated and can change ROOT
-
-
-
-    //
-    // // Check if we should show onboarding message
-    // if(this.props.onboard.get('currentStep') == 0){
-    //   // then increment the step, so we see a pop up
-    //   this.props.dispatch(onboardActions.increment());
-    // }
-
-
-
-
-    // timer.setTimeout( 'loginTimeout',
-    //   () => {
-    //     this.setState({authResponse:'something went wrong, probably no internet'})
-    //   },5000
-    // );
+    // Now pulse at least 2 times before proceeding to the login page
+    timer.setInterval(this, 'pulseInterval', () => {
+      this.pulseOnce();
+      // console.log('Pulse Interval')
+    } , 3000);
 
   }
 
 }
 
 const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    paddingTop: 130,
+    backgroundColor: GlobalStyle.constants.colors[0],
+    alignItems:'center'
+  },
   text: {
     textAlign: 'center',
     fontSize: 18,
@@ -122,7 +162,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     // textAlign: 'center',
 
-    marginTop:50,
+    marginTop:10,
   },
   buttonText: {
     textAlign: 'center',
@@ -131,8 +171,23 @@ const styles = StyleSheet.create({
     paddingBottom:15,
     color: '#fff',//Style.constants.colors[0],
 
+  },
+
+  // animation styles
+
+  animationContainer: {
+    // borderWidth:1,
+    // borderColor:'red',
+    height:150,
+
   }
 });
+
+
+LoginScreen.defaultProps = {
+  hasPulse: true,
+  growTo:1.2,
+}
 
 // which props do we want to inject, given the global state?
 function mapStateToProps(state) {
