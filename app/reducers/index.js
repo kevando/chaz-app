afd0eae8794f8a1efb071e33f8754c387b5b31c7
kevando <kevin@frameri.com>
2016-09-02 21:29:52 -0400

    removing old js code

--- app/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import chaz from './chazReducer';
-
-export {
-  chaz
-};
--- app/reducers_old/firebase/constants.js
+++
@@ -1,6 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDeWjn0xbocuyxX87PXQKPGbT9FlVNv6Xo", // works for auth. not 100% sure how i got this tho
-  authDomain: "chaz-app.firebaseapp.com",
-  databaseURL: "https://chaz-app.firebaseio.com",
-  storageBucket: "",
-};
