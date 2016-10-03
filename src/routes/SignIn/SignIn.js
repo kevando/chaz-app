b3124edd425f6f9a0645eeb79e8221122c58c4da
kevando <kevin@frameri.com>
2016-10-03 00:05:18 -0400

    basic app layout looks good. changed the login page a bit. thats it

---
+++ src/routes/SignIn/SignIn.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+import Button from '../../components/Button';
+import GenericTextInput, { InputWrapper } from '../../components/GenericTextInput';
+import images from '../../config/images';
+import styles from './styles';
+
+const SignIn = (props) => {
+  const { updateState, signIn, createAccount, getStarted, error, confirmPasswordVisible } = props;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.logo}
+          source={images.logo}
+        />
+
+      <Text style={styles.headerText}>Welcome to chaz</Text>
+        <Text style={styles.subHeaderText}>the app</Text>
+      </View>
+
+      <InputWrapper>
+        <GenericTextInput
+          placeholder="username"
+          onChangeText={(username) => updateState({ username })}
+        />
+        {confirmPasswordVisible ?
+          <GenericTextInput
+            placeholder="confirm password"
+            onChangeText={(confirmPassword) => updateState({ confirmPassword })}
+            secureTextEntry
+            borderTop
+          />
+        : null}
+      </InputWrapper>
+
+      <View style={styles.error}>
+        <Text style={styles.errorText}>{error}</Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <Button text="Get Started" onPress={getStarted} />
+      </View>
+
+      <KeyboardSpacer />
+    </View>
+  );
+};
+
+SignIn.propTypes = {
+  updateState: React.PropTypes.func,
+  signIn: React.PropTypes.func,
+  createAccount: React.PropTypes.func,
+  getStarted: React.PropTypes.func,
+  error: React.PropTypes.string,
+  confirmPasswordVisible: React.PropTypes.bool,
+};
+
+export default SignIn;
---
+++ src/routes/SignIn/SignInContainer.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { LayoutAnimation } from 'react-native';
+import Meteor, { Accounts } from 'react-native-meteor';
+import SignIn from './SignIn';
+
+class SignInContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.mounted = false;
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      confirmPasswordVisible: false,
+      error: null,
+    };
+  }
+
+  componentWillMount() {
+    this.mounted = true;
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  handleError(error) {
+    this.mounted && this.setState({ error });
+  }
+
+  validInput(overrideConfirm) {
+    const { email, password, confirmPassword, confirmPasswordVisible } = this.state;
+    let valid = true;
+
+    if (email.length === 0 || password.length === 0) {
+      this.handleError('Email and password cannot be empty.');
+      valid = false;
+    }
+
+    if (!overrideConfirm && confirmPasswordVisible && password !== confirmPassword) {
+      this.handleError('Passwords do not match.');
+      valid = false;
+    }
+
+    if (valid) {
+      this.handleError(null);
+    }
+
+    return valid;
+  }
+
+  handleSignIn() {
+    // if (this.validInput(true)) {
+      const { username, email, password } = this.state;
+      Meteor.loginWithPassword(username, username, (err) => {
+        if (err) {
+          console.log('signin err',err)
+          this.handleError(err.reason);
+        }
+      });
+    // }
+  }
+
+  // handleCreateAccount() {
+  //   const { email, password, confirmPasswordVisible } = this.state;
+  //
+  //   if (confirmPasswordVisible && this.validInput()) {
+  //     Accounts.createUser({ email, password }, (err) => {
+  //       if (err) {
+  //         this.handleError(err.reason);
+  //       } else {
+  //         // hack because react-native-meteor doesn't login right away after sign in
+  //         this.handleSignIn();
+  //       }
+  //     });
+  //   } else {
+  //     LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+  //     this.setState({ confirmPasswordVisible: true });
+  //   }
+  // }
+  handleGetStarted() {
+    const { username, email, password, confirmPasswordVisible } = this.state;
+
+
+      Accounts.createUser({ username, password: username }, (err) => {
+        if (err) {
+          console.log('getstarted err',err)
+          this.handleError(err.reason);
+          // if there is an error, we are going to assume the user exists
+          this.handleSignIn();
+        } else {
+          // hack because react-native-meteor doesn't login right away after sign in
+          this.handleSignIn();
+        }
+      });
+
+  }
+
+  render() {
+    return (
+      <SignIn
+        updateState={this.setState.bind(this)}
+        getStarted={this.handleGetStarted.bind(this)}
+        {...this.state}
+      />
+    );
+  }
+}
+
+export default SignInContainer;
