3f0825fda82da8355beac163fce3a49dc83009a8
kevando <kevin@frameri.com>
2016-10-05 01:32:43 -0400

    mostly removing config for fabric and some other bullshit, we are good!

--- ios/chaz/AppDelegate.m
+++ ios/chaz/AppDelegate.m
@@ -12,90 +12,26 @@
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
 
-#import <Fabric/Fabric.h>
-#import <Crashlytics/Crashlytics.h>
-
-// Adding for additional js logging in fabric
-#import <asl.h>
-#import "RCTLog.h"
-
-// @import Firebase;
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
 
-
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
-  // I guess we dont use these anymore
-  // jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-  // jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"chaz"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  // rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-
-
-  // [FIRApp configure];
-
-  [Fabric with:@[[Crashlytics class]]];
-  //Add the following lines for additional js debugging
-  RCTSetLogThreshold(RCTLogLevelInfo);
-  RCTSetLogFunction(CrashlyticsReactLogFunction);
-
   return YES;
 }
-
-
-RCTLogFunction CrashlyticsReactLogFunction = ^(
-                                         RCTLogLevel level,
-                                         __unused RCTLogSource source,
-                                         NSString *fileName,
-                                         NSNumber *lineNumber,
-                                         NSString *message
-                                         )
-{
-    NSString *log = RCTFormatLog([NSDate date], level, fileName, lineNumber, message);
-
-    #ifdef DEBUG
-        fprintf(stderr, "%s\n", log.UTF8String);
-        fflush(stderr);
-    #else
-        CLS_LOG(@"REACT LOG: %s", log.UTF8String);
-    #endif
-
-    int aslLevel;
-    switch(level) {
-        case RCTLogLevelTrace:
-            aslLevel = ASL_LEVEL_DEBUG;
-            break;
-        case RCTLogLevelInfo:
-            aslLevel = ASL_LEVEL_NOTICE;
-            break;
-        case RCTLogLevelWarning:
-            aslLevel = ASL_LEVEL_WARNING;
-            break;
-        case RCTLogLevelError:
-            aslLevel = ASL_LEVEL_ERR;
-            break;
-        case RCTLogLevelFatal:
-            aslLevel = ASL_LEVEL_CRIT;
-            break;
-    }
-    asl_log(NULL, NULL, aslLevel, "%s", message.UTF8String);
-
-
-};
 
 @end
