07c1240aafe83f42ff587e277d09b5f476c176d9
kevando <kevin@frameri.com>
2016-10-11 22:34:24 -0400

    moved and deleted some old files

---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../config/styles';
+
+export default styles = StyleSheet.create({
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: colors.buttonBackground,
+    margin: 5,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600',
+    paddingTop: 5
+  },
+});
